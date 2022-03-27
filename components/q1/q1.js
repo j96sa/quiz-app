570ad73e01b67f56f64758248e8f5a5fcc992e90
Josue Paulo Antuan <josue96sa@gmail.com>
2022-03-27 19:49:47 +0200

    feat(q1):user-info
    *implementada la forma el la que se actualiza la informacion del
    usuario, implementado tambien una validacion para saber si se ha
    seleccionado la respuesta correcta
    
    :/establecer un limite de tiempo para responder que cuando sea exedido
    finalice con la pregunta
    :/estilizar el componente
    :/cuando se seleccione la respuesta de be de salir una ventana emergente
    con la puntuacion obtenida, el tiempo que se ha tardado en responder, y
    debe de tener la opcion de continuar o parar el quest.

--- components/q1/q1.js
+++ components/q1/q1.js
@@ -1,46 +1,106 @@
 const d = document;
 
+//Array donde se alamcenan los titulos de los libros
+const BooksArr = [
+    {book:"El Viejo y el Mar"},
+    {book:"Romeo y Julieta"},
+    {book:"Shining"},
+    {book:"Don Quijote de la Mancha"},
+    {book:"El Gato Negro"}
+];
 
+//variable para conectar el libro con el autor y saber si se ha seleccionado la opcion correcta
+const BOOK_INDEX = Math.floor(Math.random()*BooksArr.length);
+
+
+//VARIABLES CON LOS DATOS PARA MODIFICAR LA INFORMACION DEL USUARIO
+const startTime = Date.now();
+let score = 0;
+
+/* * * * CONSTANTE PARA EXPORTAR EL COMPONENTE * * * */
 export const Q1 = ()=>{
-    RenderQ1("send-data")
-    change();    
+    RenderQ1(BOOK_INDEX);
+    questionValidation(BOOK_INDEX);  
 };
 
-const change = ()=>{
+/* CONSTANTE PARA SABER SI SE HA SELECCIONADO LA RESPUESTA CORRECTA
+Y CON TODA LA LOGICA DEL QUEST DEL COMPONENTE */
+const questionValidation = (bookIndex)=>{
     d.addEventListener("click",e=>{
-        if(e.target === d.querySelector(".btn")){
-            const userArray = JSON.parse(localStorage.getItem("leaderboard")),
+        if(e.target.matches("li") && e.target.dataset.response === BooksArr[bookIndex].book){
             
-            userID = (location.hash).substring(5),
-            $time = d.querySelector(".form .time").value,
-            $score = d.querySelector(".form .score").value;
+            //para saber cuanto tiempo ha transcurrido desde que inicio el test hasta que ha sido respondido
+            const finalTime = Math.round((Date.now() - startTime)/1000);
             
-            userArray.forEach(user=>{
-                if(user.id === userID){
-                    
-                    user.questions.q1.score = $score;
-                    user.questions.q1.time = $time;                    
-                    
-                    localStorage.setItem("leaderboard",JSON.stringify(userArray));
-                };
-            });
+            if(finalTime < 11){
+                score = 5;
+            }else if(finalTime < 31){
+                score = 4;
+            }else if(finalTime < 51){
+                score = 3;
+            }else if(finalTime < 81){
+                score = 2;
+            }else{
+                score = 1;
+            }
+            userInfoMod(finalTime,score)
+
+        }else if(e.target.matches("li") && e.target.dataset.response !== BooksArr[bookIndex].book){
+            userInfoMod(0,0);
         };
     });
+};  
+
+
+//COSNTANTE PARA MODIFICAR LA INFORMACION DEL USUARIO
+const userInfoMod = (time,score)=>{        
+    console.log(time);
+    console.log(score)
+    
+    const userArray = JSON.parse(localStorage.getItem("leaderboard"));
+    
+    const userID = (location.hash).substring(5);
+    
+    userArray.forEach(user=>{
+        if(user.id === userID){
+            
+            user.questions.q1.score = score;
+            user.questions.q1.time = time;                    
+            
+            localStorage.setItem("leaderboard",JSON.stringify(userArray));
+        };
+    });
+        
 };
 
 
 
 /* RENDERIZADO DEL COMPONENTE */
-const RenderQ1 = (value)=>{
+const RenderQ1 = (bookIndex)=>{
+    //OPTION 1
+    /* const arr = [`
+    <h1>Pregunta 1</h1>
+    <h2>Cual de los siguientes escritores es el autor del libro: "Romeo y Julieta" </h2>
+    <div>
+        <li>William Shakespeare</li>
+        <li>Ernest Hemingway</li>
+        <li>Stephen King</li>
+        <li>Jorge Luis Borges</li>
+    </div>
+    `,
+    
+    `q1.2`]; */
+
     d.getElementById("root").innerHTML = `
-    <h1 style="color:#f00">Q1-v3.3</h1>
-    
-    <form class="form">
-    <input class="time" type="text" placeholder="tempo...">
-    <input class="score" type="number" placeholder="puntuazione...">            
-    </form>
-    
-    <button class="btn">${value}</button>
+    <h1>Pregunta 1</h1>
+    <h2>Cual de los siguientes escritores es el autor del libro:"${BooksArr[bookIndex].book}"</h2>
+    <div>
+        <li data-response="${BooksArr[1].book}">William Shakespeare</li>
+        <li data-response="${BooksArr[0].book}">Ernest Hemingway</li>
+        <li data-response="${BooksArr[2].book}">Stephen King</li>
+        <li data-response="${BooksArr[3].book}">Miguel de Cervantes</li>
+        <li data-response="${BooksArr[4].book}">Edgar Allan Poe</li>
+    </div>
     `;
 };
 
